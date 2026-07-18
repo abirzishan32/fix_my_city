f394cc76ff65d4d7ccacbdf87826d01f5aa56dde
Abir Rahman <118625188+abirzishan32@users.noreply.github.com>
2026-07-18 21:41:29 +0600

    Proper comments on UserRepository.java

--- backend/src/main/java/com/example/fixmycity/repository/UserRepository.java
+++ backend/src/main/java/com/example/fixmycity/repository/UserRepository.java
@@ -5,9 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+
+// Create a repository for User entities where the primary key type is Long
+/*
+Syntax: JpaRepository<EntityType, IDType>
+Here:
+EntityType = User
+IDType     = Long
+*/
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email); // This method will allow us to find a user by their email address.
+                                                // Instead of returning User, it returns an Optional<User> which is a container object which may or may not contain a non-null value.
 
-    boolean existsByEmail(String email);
+    boolean existsByEmail(String email);  // Checks if a user with the given email exists in the database. Returns true if a user with the given email exists, false otherwise.
 }
